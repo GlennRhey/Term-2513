c3f7d8ddb8db9ea6b2f249a77d51903a9ff08f2b
GlennRhey <glennorillos321@gmail.com>
2025-09-23 06:44:42 +0800

    Add new files

---
+++ index.js
@@ -0,0 +1,38 @@
+//2
+let isDone = true
+let age = 21
+let school = "AMA"
+let price = 40.12
+
+//3
+console.log(isDone);
+console.log(age);
+console.log(school);
+console.log(price);
+
+//4
+console.log(typeof(isDone));
+console.log(typeof(age));
+console.log(typeof(school));
+console.log(typeof(price));
+
+//5
+alert("welcome to my page");
+
+//6
+let Num1st = prompt("Enter a number: ");
+let Num2nd = prompt("Enter a number again: ");
+
+//7
+Num1st = Number(Num1st);
+Num2nd = Number(Num2nd);
+
+//8
+(Num1st - Num2nd);
+
+//9
+console.log(Num1st - Num2nd);
+
+//10
+console.log(typeof(Num1st));
+console.log(typeof(Num2nd));
